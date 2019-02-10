561cdb51c8cb1b1f07bc652e0ff80ee2edcba248
OnLiU <public.danuel@gmail.com>
2019-02-10 21:40:46 +0900

    [add] JavaScript x Vue

---
+++ javascript-vue/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Human></Human>
+</template>
+
+<script>
+import Human from "./Human.vue";
+
+export default {
+  components: {
+    Human
+  }
+};
+</script>
---
+++ javascript-vue/Human.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>{{id}}</div>
+    <div>{{name}}</div>
+    <div>{{age}}</div>
+    <div>{{description}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Human",
+  data() {
+    return {
+      id: "@human",
+      name: "human",
+      age: 17,
+      description: "New Human"
+    };
+  }
+};
+</script>
