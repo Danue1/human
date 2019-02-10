4fb1f422ff2b4775e0269f1b2234783a52527bd0
OnLiU <public.danuel@gmail.com>
2019-02-10 22:01:09 +0900

    [add] Kotlin

---
+++ kotlin/Human.kt
@@ -0,0 +1,8 @@
+package Huamn
+
+data class Human (
+  val id: String,
+  val name: String,
+  val age: Int,
+  val description: String
+)
---
+++ kotlin/main.kt
@@ -0,0 +1,14 @@
+package Human
+
+import Human
+
+val HUMAN = Human(
+  id = "@_danuel_",
+  name = "Danuel",
+  age = 17,
+  description = "best code monkey"
+)
+
+fun main (args: Array<String>) {
+  println(HUMAN)
+}
